fcbd3506ed2f0e66ab6d63886115b9d12daa3555
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-20 20:30:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
-class Dog  #belong to a shelter & an owner
-  attr_accessor :breed
-  
-  def initialize(breed)
-  
-    @breed = breed 
-    
-  end 
-  
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
   def breed
-    @breed = breed
+    @breed
   end 
 end 
